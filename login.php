f5d1e9a95630437f3f3a61c71ba4a5e3b4c92874
hansangyeob <57244611+hansangyeob@users.noreply.github.com>
2020-06-20 18:07:00 +0700

    Initial commit

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Summer project</title>
+</head>
+<body>
+<div style="left:10rem;">
+    <h1>This is for Log in</h1>
+        <h3>ID:</h3> 
+        <input type="text" > 
+    <br>
+        <h3>Password:</h3> 
+        <input type="text" >
+    <br>
+    <br>
+    <button>Login</button>
+</div>
+
+</body>
+</html>
