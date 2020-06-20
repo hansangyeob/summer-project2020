dea59a7ebe21b7eec801c80a658623edbdcaefd6
hansangyeob <57244611+hansangyeob@users.noreply.github.com>
2020-06-20 18:14:35 +0700

    login session

--- login.php
+++ login.php
@@ -6,17 +6,19 @@
     <title>Summer project</title>
 </head>
 <body>
-<div style="left:10rem;">
-    <h1>This is for Log in</h1>
-        <h3>ID:</h3> 
-        <input type="text" > 
+<form action="process.php" method="post" name="Login_Form">
+    <div style="left:10rem;">
+        <h1>This is for Log in</h1>
+            <h3>ID:</h3> 
+            <input type="text" placeholder="Enter your ID" > 
     <br>
-        <h3>Password:</h3> 
-        <input type="text" >
+            <h3>Password:</h3> 
+            <input type="password" placeholder="Enter your password" >
     <br>
     <br>
-    <button>Login</button>
+        <button>Login</button>
 </div>
+</form>
 
 </body>
 </html>
